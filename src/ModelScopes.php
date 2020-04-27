afd33c823f09d8fa05e9742c4a1dc180bb00da66
DR.H2S04 <pratikdai404@gmail.com>
2020-04-27 15:51:57 +0545

    v_1

--- src/ModelScopes.php
+++ src/ModelScopes.php
@@ -3,32 +3,63 @@ namespace drh2so4\ModelScope;
 
 trait ModelScopes
 {
-    /* ******************************** For all models ******************************** */
+    public function scopeGetLatestAll($query){
+        return $query->orderBy('updated_at','ASC')->get();
+    }
 
-
-
-    public function scopeLatestGet($query,$limit){
-      
+    public function scopeGetLatest($query,$limit){
         return $query->orderBy('updated_at','ASC')->take($limit)->get();
     }
 
-    public function scopeLatestAll($query){
+    public function scopeLastWeekData($query){
+        $date = \Carbon\Carbon::today()->subDays(7);
+    $query->where('updated_at','>=',$date)->get();
+    }
+
+    public function scopeLastWeekDataLimit($query,$limit = 5){
+        $date = \Carbon\Carbon::today()->subDays(7);
+    $query->where('updated_at','>=',$date)->take($limit)->get();
+    }
+
+    public function scopeLastMonthData($query){
+        $date = \Carbon\Carbon::today()->subMonth();
+        $query->where('updated_at','>=',$date)->get();
+    }
+
+    public function scopeLastMonthDataLimit($query,$limit = 5){
+        $date = \Carbon\Carbon::today()->subMonth();
+        $query->where('updated_at','>=',$date)->take($limit)->get();
+    }
+
+    public function scopeTodayData($query){
+        $date = \Carbon\Carbon::today();
+        $query->where('updated_at',$date)->get();
+    }
+
+    public function scopeAsc($query){
         return $query->orderBy('updated_at','ASC')->get();
     }
 
-
-    public function scopeTest($query){
-        return $query->orderBy('updated_at','ASC')->get();
+    public function scopeDesc($query){
+        return $query->orderBy('updated_at','DESC')->get();
     }
 
- 
+    public function scopeFilter($query, array $filterData = [])
+    {
+        foreach ($filterData as $key => $value) {
+   
 
-    /* ******************************************************************************** */
+            if (is_null($value) || $value === '') continue;
 
-    public function scopeLatestPublishedGet($query,$limit){
-        return $query->orderBy('updated_at','ASC')->where('status','PUBLISHED')->take($limit)->get();
-    }
-    public function scopeLatestImageShowGet($query,$limit){
-        return $query->orderBy('updated_at','ASC')->where('image_show',1)->take($limit)->get();
+            $scopeName = ucfirst(str::of($key)->camel());
+
+            if (method_exists($this, 'scope' . $scopeName)) {
+                $query->$scopeName($value);
+            } else if (is_array($value)) {
+                $query->whereIn($key, $value);
+            } else {
+                $query->where($key, $value);
+            }
+        }
     }
 }
