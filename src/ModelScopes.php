dd975ceaf396b413778f3eb1f153031d347534a5
DR.H2S04 <pratikdai404@gmail.com>
2020-04-27 14:34:11 +0545

    initial commit

---
+++ src/ModelScopes.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Traits;
+
+trait ModelScopes
+{
+    /* ******************************** For all models ******************************** */
+
+
+
+    public function scopeLatestGet($query,$limit){
+      
+        return $query->orderBy('updated_at','ASC')->take($limit)->get();
+    }
+
+    public function scopeLatestAll($query){
+        return $query->orderBy('updated_at','ASC')->get();
+    }
+
+
+    public function scopeTest($query){
+        return $query->orderBy('updated_at','ASC')->get();
+    }
+
+ 
+
+    /* ******************************************************************************** */
+
+    public function scopeLatestPublishedGet($query,$limit){
+        return $query->orderBy('updated_at','ASC')->where('status','PUBLISHED')->take($limit)->get();
+    }
+    public function scopeLatestImageShowGet($query,$limit){
+        return $query->orderBy('updated_at','ASC')->where('image_show',1)->take($limit)->get();
+    }
+}
